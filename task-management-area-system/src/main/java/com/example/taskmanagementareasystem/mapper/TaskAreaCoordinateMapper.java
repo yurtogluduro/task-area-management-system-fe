78be1f78cff9bc27bd38226454b5b824f2f4acc7
yurtogluduro <durmus.yurtogluu@gmail.com>
2026-04-22 23:40:10 +0400

    file clearing

--- task-management-area-system/src/main/java/com/example/taskmanagementareasystem/mapper/TaskAreaCoordinateMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.taskmanagementareasystem.mapper;
-
-import com.example.taskmanagementareasystem.dto.TaskAreaCoordinateDto;
-import com.example.taskmanagementareasystem.entity.TaskAreaCoordinateEntity;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface TaskAreaCoordinateMapper extends BaseMapper<TaskAreaCoordinateDto, TaskAreaCoordinateEntity> {
-}
